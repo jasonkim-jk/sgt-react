c2cd75c9edb1237556146b1bcaec1f64c5029339
Jason Kim <thehesedlife@gmail.com>
2020-07-16 21:13:12 -0700

    added GradeTable component - created GradeTable and imported Grade component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -4,7 +4,14 @@ import Header from './header';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { grades: [] };
+  }
+
+  componentDidMount() {
+    fetch('/api/grades')
+      .then(response => response.json())
+      .then(data => this.setState({ grades: data }))
+      .catch(error => console.error(error.message));
   }
 
   render() {
---
+++ client/components/grade-table.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Grade from './grade';
+
+export default function GradeTable(props) {
+  const gradeList = (
+    props.grades.map(student => <Grade grade={student} key={student.id} />)
+  );
+
+  return (
+    <table className='table'>
+      <thead className='thead-light'>
+        <tr>
+          <th scope='col'>Student Name</th>
+          <th scope='col'>Course</th>
+          <th scope='col'>Grade</th>
+        </tr>
+      </thead>
+      <tbody>
+        { gradeList }
+      </tbody>
+    </table>
+  );
+}
