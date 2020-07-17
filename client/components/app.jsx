ab9f14682b06e442133b162cec587a256579907d
Jason Kim <thehesedlife@gmail.com>
2020-07-16 20:37:36 -0700

    added some components - added header component to display a table title

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Header from './header';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    return null;
+    return (
+      <Header title='Student Grade Table'/>
+    );
   }
 }
 
